c5cb18d172d63e0ec91aa1c4237b1a9c3855d58d
AbhinavSharma486 <abhinavparashar486@gmail.com>
2025-02-19 01:34:22 +0530

    user model complete

---
+++ backend/models/user.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlenth: 6
+    },
+    profilePic: {
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+    },
+    isVerified: {
+      type: Boolean,
+      default: false
+    },
+
+    resetPasswordToken: String,
+
+    resetPasswordTokenExpiresAt: Date,
+
+    verificationToken: String,
+
+    verificationTokenExpiresAt: Date,
+
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
