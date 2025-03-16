707fd15fb8d0ad9e327d664ce40bb2880ef19ea3
AbhinavSharma486 <abhinavparashar486@gmail.com>
2025-03-17 00:16:57 +0530

    change in OAUTH

--- frontend/src/components/OAuth.jsx
+++ frontend/src/components/OAuth.jsx
@@ -20,7 +20,7 @@ export default function OAuth() {
 
     try {
       const resultsFromGoogle = await signInWithPopup(auth, provider);
-      const res = await fetch('http://localhost:5001/api/auth/google', {
+      const res = await fetch('/api/auth/google', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
