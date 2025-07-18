e94b1b4043dfa8daf4f97b3c8de9c755e007a7ba
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2025-07-19 00:08:26 +0530

    deleteMessage controller & route complete

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -171,3 +171,41 @@ export const editMessage = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const deleteMessage = async (req, res) => {
+  try {
+    const { messageId } = req.params;
+    const userId = req.user.id;
+    const message = await Message.findById(messageId);
+
+    if (!message) return res.status(404).json({ message: "Message not found" });
+
+    if (message.senderId.toString() !== userId) return res.status(403).json({ message: "Unauthorized" });
+
+    // soft delete: replace text with 'Message deleted'
+    message.text = "Message deleted";
+    message.image = undefined;
+    message.edited = false;
+    message.editedAt = undefined;
+
+    await message.save();
+
+    // Real time update
+    const receiverSocketIds = getReceiverSocketId(message.receiverId.toString());
+
+    if (receiverSocketIds) {
+      const socketIdsArray = Array.isArray(receiverSocketIds) ? receiverSocketIds : [receiverSocketIds];
+
+      socketIdsArray.forEach(socketId => {
+        io.to(socketId).emit("messageDeleted", { messageId: message._id });
+      });
+    }
+
+    io.to(req.user.id).emit("messageDeleted", { messageid: message._id });
+
+    res.status(200).json({ messageId: message._id });
+  } catch (error) {
+    console.log("Error in deleteMessage controller", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { editMessage, getMessages, getUsersForSidebar, reactToMessage, sendMessage } from "../controllers/message.controller.js";
+import { deleteMessage, editMessage, getMessages, getUsersForSidebar, reactToMessage, sendMessage } from "../controllers/message.controller.js";
 
 
 const router = express.Router();
@@ -15,5 +15,7 @@ router.post("/send/:id", protectRoute, sendMessage);
 router.post("/react/:messageId", protectRoute, reactToMessage);
 
 router.put("/edit/:messageId", protectRoute, editMessage);
+
+router.delete("/delete/:messageId", protectRoute, deleteMessage);
 
 export default router;
